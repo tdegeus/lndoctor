8ebb36bb14f32543eb0cd856ad62e27625b5f772
Tom de Geus <tom@geus.me>
2021-07-05 09:59:13 +0200

    Adding test

--- test/main.py
+++ test/main.py
@@ -14,10 +14,46 @@ class Test_cli(unittest.TestCase):
     def test_realpath(self):
 
         dirname, filename = os.path.split(os.path.abspath(__file__))
-        os.symlink(os.path.join(dirname, filename), os.path.join(dirname, "mylink"))
-        path = run("lndoctor -r {0:s}".format(os.path.join(dirname, "mylink")))
+        a = os.path.join(dirname, "mylink")
+
+        for i in [a]:
+            if os.path.islink(i):
+                os.remove(i)
+            elif os.path.exists(i):
+                raise IOError("Cannot run test: '{0:s}' exists".format(i))
+
+        os.symlink(os.path.join(dirname, filename), a)
+
+        path = run("lndoctor -r {0:s}".format(a))
         self.assertEqual(path.strip(), os.path.join(dirname, filename))
-        os.remove(os.path.join(dirname, "mylink"))
+
+        os.remove(a)
+
+    def test_recursive(self):
+
+        dirname, filename = os.path.split(os.path.abspath(__file__))
+        a = os.path.join(dirname, "mylink")
+        b = os.path.join(dirname, "mylink2")
+
+        for i in [a, b]:
+            if os.path.islink(i):
+                os.remove(i)
+            elif os.path.exists(i):
+                raise IOError("Cannot run test: '{0:s}' exists".format(i))
+
+        os.symlink(os.path.join(dirname, filename), a)
+        os.symlink(a, b)
+
+        ret = run("lndoctor --colors none {0:s}".format(os.path.join(dirname, "mylink2")))
+        ret = list(filter(None, ret.split("\n")))
+        ret = [i[:-1] if i[-1] == "/" else i for i in ret]
+
+        self.assertTrue(len(ret) == 2)
+        self.assertEqual(ret[1], a)
+        self.assertEqual(ret[0], b)
+
+        os.remove(a)
+        os.remove(b)
 
 
 if __name__ == '__main__':
